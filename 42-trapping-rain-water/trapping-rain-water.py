95f58420edec54585122ae9ff8b2923fbce2cecc
Nishant Rathod <nickrathod977@gmail.com>
2026-01-15 00:10:37 +0530

    Time: 43 ms (6.62%) | Memory: 14.2 MB (13.57%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -4,17 +4,15 @@ class Solution(object):
         n = len(height)
         max_left = [0] * n
         max_right = [0] * n
-        
+
         for i in range(n):
-            j = -i - 1                      
+            j = -i -1
             max_left[i] = l_wall
             max_right[j] = r_wall
-            l_wall = max(l_wall, height[i])
-            r_wall = max(r_wall, height[j])
-        
+            l_wall = max(l_wall,height[i])
+            r_wall = max(r_wall,height[j])
         summ = 0
         for i in range(n):
-            pot = min(max_left[i], max_right[i])
-            summ += max(0, pot - height[i])
-        
+            pot = min(max_left[i],max_right[i])
+            summ += max(0,pot - height[i])
         return summ
