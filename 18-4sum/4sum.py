7dd84204810addf13d5aa567d6cfd177e46ef127
Nishant Rathod <nickrathod977@gmail.com>
2026-01-19 23:23:20 +0530

    Time: 67 ms (92.10%) | Memory: 12.5 MB (57.86%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -1,37 +1,34 @@
 class Solution(object):
     def fourSum(self, nums, target):
         nums.sort()
-        res = []
+        if len(nums) < 4: return []
         n = len(nums)
-
-        for i in range(n - 3):
-            if i > 0 and nums[i] == nums[i - 1]:
+        res= []
+        l ,r = 0, n-1
+        for i in range(n-3):
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
-
-            for j in range(i + 1, n - 2):
-                if j > i + 1 and nums[j] == nums[j - 1]:
+            if nums[i] + nums[i+1] + nums[i+2] + nums[i+3] > target:
+                break
+            if nums[i] + nums[n-1] + nums[n-2] + nums[n-3] < target:
+                continue
+            k = target - nums[i]
+            for j in range(i+1,n-2):
+                if j > i+1 and nums[j] == nums[j-1]:
                     continue
-
-                left = j + 1
-                right = n - 1
-
-                while left < right:
-                    total = nums[i] + nums[j] + nums[left] + nums[right]
-
-                    if total == target:
-                        res.append([nums[i], nums[j], nums[left], nums[right]])
-
-                        left += 1
-                        right -= 1
-
-                        while left < right and nums[left] == nums[left - 1]:
-                            left += 1
-                        while left < right and nums[right] == nums[right + 1]:
-                            right -= 1
-
-                    elif total < target:
-                        left += 1
+                l = j+1
+                r = n-1
+                h = k - nums[j]
+                while l<r:
+                    if nums[l] +nums[r] == h:
+                        res.append([nums[i],nums[j],nums[l],nums[r]])
+                        while l< r and nums[l] == nums[l+1]:
+                            l+=1
+                        while l < r and nums[r] == nums[r-1]:
+                            r-=1
+                        l+=1
+                    elif nums[l] + nums[r] > h:
+                        r-=1
                     else:
-                        right -= 1
-
+                        l+=1
         return res
