ceb499699e0c76ac1940f597be8387db5c926fa9
Nishant Rathod <nickrathod977@gmail.com>
2026-05-11 23:57:03 +0530

    Time: 13 ms (55.28%) | Memory: 12.6 MB (93.50%) - LeetSync

---
+++ 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def separateDigits(self, nums):
+        ans = []
+        for num in nums:
+            for digit in str(num):
+                ans.append(int(digit))
+        return ans
