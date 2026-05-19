b6b884cd7e888b1f838c12afec4e07122a39ad49
Nishant Rathod <nickrathod977@gmail.com>
2026-05-19 17:59:29 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (8.74%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.py
+++ 58-length-of-last-word/length-of-last-word.py
@@ -2,14 +2,12 @@ class Solution(object):
     def lengthOfLastWord(self, s):
         n = len(s)
         cnt = 0
-        spaceCnt = 0
         for i in range(n-1,-1,-1):
             x = ord(s[i])
             if x == 32 and cnt==0:
                 continue
             if (x >= 65 and x <= 90) or (x >= 97 and x <= 122):
                 cnt += 1
-                spaceCnt = 1
             else:
                 break
         return cnt
