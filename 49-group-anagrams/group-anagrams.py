5d0f348ef62e9d86dcf2b886f786a7f9932f1613
Nishant Rathod <nickrathod977@gmail.com>
2026-05-21 18:21:16 +0530

    Time: 15 ms (96.86%) | Memory: 15.9 MB (97.56%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        dict1 = {}
+
+        for s in strs:
+            key = "".join(sorted(s))
+
+            if key in dict1:
+                dict1[key].append(s)
+            else:
+                dict1[key] = [s]
+
+        return list(dict1.values())
