5d113a0ed33cbc304cbdfdd828d99b8b1f41981a
Nishant Rathod <nickrathod977@gmail.com>
2026-06-01 21:21:49 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (60.05%) - LeetSync

---
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def middleNode(self, head):
+        length = 0
+        curr = head
+
+        while curr != None:
+            curr = curr.next
+            length += 1
+
+        curr = head
+        for i in range(length//2):
+            curr = curr.next
+        return curr        
