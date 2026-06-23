4560967234ab638033ed15f09b7a8c5c3b003365
Nishant Rathod <nickrathod977@gmail.com>
2026-06-23 15:55:25 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (52.87%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,21 +1,13 @@
 class Solution(object):
     def isHappy(self, n):
-        arr = []
-        
-        while n not in arr:
-            arr.append(n)
-
-            total = 0
-            x = n
-
-            while x > 0:
-                d = x % 10
-                total += d * d
-                x //= 10
-
-            if total == 1:
-                return True
-
-            n = total
-
-        return False
+        while n!=1:
+            ans = 0
+            while n!=0:
+                d = n % 10
+                sq = d * d
+                ans += sq
+                n //= 10
+            n = ans
+            if n==4:
+                return False
+        return True
