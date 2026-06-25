f9f602f8e390386daa8feb8a1ebccda19ce6cf99
Nishant Rathod <nickrathod977@gmail.com>
2026-06-25 14:14:13 +0530

    Time: 13 ms (81.96%) | Memory: 24.4 MB (91.86%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,14 +1,8 @@
 class Solution(object):
     def containsDuplicate(self, nums):
-        n = len(nums)
         freq = {}
-        for i in range(n):
-            if nums[i] not in freq:
-                freq[nums[i]] = 1
-            else:
-                freq[nums[i]] += 1
-        for key in freq:
-            if freq[key] >= 2:
+        for num in nums:
+            if num in freq:
                 return True
-
+            freq[num] = 1
         return False
