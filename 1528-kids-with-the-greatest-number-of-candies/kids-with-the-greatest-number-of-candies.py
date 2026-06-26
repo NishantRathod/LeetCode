493c75e7147ba239b539bc5735b37953406a9eba
Nishant Rathod <nickrathod977@gmail.com>
2026-06-26 21:52:19 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (53.51%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
@@ -1,17 +1,10 @@
 class Solution(object):
     def kidsWithCandies(self, candies, extraCandies):
-        n = len(candies)
-        ans = []
-        for i in range(n):
-            flag = True
-            addCandie = 0
-            for j in range(n):
-                if i != j:
-                    addCandie = candies[i] + extraCandies
-                    if addCandie >= candies[j]:
-                        flag = True 
-                    else:
-                        flag = False
-                        break 
-            ans.append(flag)
-        return ans         # [2,3,5,1,3]
+        a=max(candies)
+        ans=[]
+        for i in candies:
+            if (i+extraCandies)>=a:
+                ans.append(True)
+            else:
+                ans.append(False)
+        return ans
