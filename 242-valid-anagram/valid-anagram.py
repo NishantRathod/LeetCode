90d6799d386d4daef65d60a1dfd51785fb91e3e7
Nishant Rathod <nickrathod977@gmail.com>
2026-06-29 14:57:49 +0530

    Time: 16 ms (73.59%) | Memory: 14.4 MB (8.47%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -3,24 +3,24 @@ class Solution(object):
         n = len(s)
         m = len(t)
 
+        freq = {}
         if n != m:
             return False
 
-        freqS = {}
-
         for i in range(n):
-            if s[i] not in freqS:
-                freqS[s[i]] = 1
+            if s[i] not in freq:
+                freq[s[i]] = 1
             else:
-                freqS[s[i]] += 1
+                freq[s[i]] += 1
 
         for i in range(m):
-            if t[i] not in freqS:
+            if t[i] not in freq:
+                return False
+ 
+            freq[t[i]] -= 1
+
+            if freq[t[i]] < 0:
                 return False
 
-            freqS[t[i]] -= 1
-
-            if freqS[t[i]] < 0:
-                return False
 
         return True
