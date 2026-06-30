46f06b8263378130f41190913eb9a78c4cbb83bf
Nishant Rathod <nickrathod977@gmail.com>
2026-06-30 11:53:11 +0530

    Time: 3 ms (69.66%) | Memory: 12.3 MB (81.57%) - LeetSync

--- 415-add-strings/add-strings.py
+++ 415-add-strings/add-strings.py
@@ -1,9 +1,44 @@
 class Solution(object):
     def addStrings(self, num1, num2):
-        
-        n1 = int(num1)
-        n2 = int(num2)
+        n = len(num1)
+        m = len(num2)
 
-        ans = n1 + n2
+        i = n - 1
+        j = m - 1
 
-        return str(ans)
+        carry = 0
+        ans = ""
+
+        while i >= 0 and j >= 0:
+            d1 = ord(num1[i]) - ord('0')
+            d2 = ord(num2[j]) - ord('0')
+
+            lastDigit = (d1 + d2 + carry) % 10
+            ans += str(lastDigit)
+            carry = (d1 + d2 + carry) // 10
+
+            i -= 1
+            j -= 1
+
+        while i >= 0:
+            d1 = ord(num1[i]) - ord('0')
+
+            lastDigit = (d1 + carry) % 10
+            ans += str(lastDigit)
+            carry = (d1 + carry) // 10
+
+            i -= 1
+
+        while j >= 0:
+            d2 = ord(num2[j]) - ord('0')
+
+            lastDigit = (d2 + carry) % 10
+            ans += str(lastDigit)
+            carry = (d2 + carry) // 10
+
+            j -= 1
+
+        if carry > 0:
+            ans += str(carry)
+
+        return ans[::-1]
